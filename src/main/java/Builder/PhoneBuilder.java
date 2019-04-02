6f5ed8cfbf31f716bea8e75d67f2fc29d467247c
giciarz412 <tomaszfefler1990@gmail.com>
2019-04-02 18:31:34 +0200

    Builder return this;

--- src/main/java/Builder/PhoneBuilder.java
+++ src/main/java/Builder/PhoneBuilder.java
@@ -13,17 +13,21 @@ public class PhoneBuilder {
 
     public PhoneBuilder setRam(int ram) {
         this.ram = ram;
+        return this;
     }
 
     public PhoneBuilder setProcessor(String processor) {
         this.processor = processor;
+        return this;
     }
 
     public PhoneBuilder setScreenSize(double screenSize) {
         this.screenSize = screenSize;
+        return this;
     }
 
     public PhoneBuilder setBattery(int battery) {
         this.battery = battery;
+        return this;
     }
 }
